59719e69e3981aeff8fff9c51d16b1a1c1292a37
Bruce Grugett <bgrugett@gmail.com>
2017-03-06 12:21:06 -0500

    bruce

--- bash.js
+++ bash.js
@@ -3,14 +3,17 @@ let commands = require('./commands');
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', function(data){
-  var cmd = data.toString().trim();
+  let input = data.toString().trim();
+  let cmdArr = input.split(' ');
+  let cmd = cmdArr.shift();
+  let args = cmdArr.join(' ');
 
-  if(Object.keys(commands).includes(cmd)){
-    commands[cmd]();
+  if (Object.keys(commands).includes(cmd)){
+    commands[cmd](args);
   }
   //process.stdout.write('You typed: ' + cmd);
 
 
-  process.stdout.write('\nprompt > ');
+  //process.stdout.write('\nprompt > ');
 
 });
--- commands.js
+++ commands.js
@@ -1,11 +1,72 @@
+var fs = require('fs');
 module.exports = {
 
   pwd: function(){
     process.stdout.write(process.cwd());
+    process.stdout.write('\nprompt > ');
   },
 
   date: function(){
     process.stdout.write(Date());
+    process.stdout.write('\nprompt > ');
+  },
+
+  echo: function(args){
+    process.stdout.write(args);
+    process.stdout.write('\nprompt > ');
+  },
+
+  cat: function(file){
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) throw err;
+      console.log(data);
+     // process.stdout.write(args);
+    process.stdout.write('\nprompt > ');
+    });
+  },
+
+  head: function(file){
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) throw err;
+      let newLineCount = 0;
+      let pos = 0;
+      while ( newLineCount < 5 ) {
+        if (data[pos] === '\n') {
+          newLineCount++;
+        }
+        pos++;
+      }
+      console.log(data.slice(0, pos));
+     // process.stdout.write(args);
+    process.stdout.write('\nprompt > ');
+    });
+  },
+
+  tail: function(file){
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) throw err;
+      let newLineCount = 0;
+      let pos = data.length - 1;
+      while ( newLineCount < 5 ) {
+        if (data[pos] === '\n') {
+          newLineCount++;
+        }
+        pos--;
+      }
+      console.log(data.slice(pos));
+     // process.stdout.write(args);
+    process.stdout.write('\nprompt > ');
+    });
+  },
+
+  ls: function() {
+    fs.readdir('.', function(err, files) {
+  if (err) throw err;
+  files.forEach(function(file) {
+    process.stdout.write(file.toString() + "\n");
+  });
+    process.stdout.write('\nprompt > ');
+});
   }
 
 };
